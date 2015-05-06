b7514ea67e2da45b9ef191979e65536ae0ca9ce6
Rui Baltazar <rui.p.baltazar@gmail.com>
2015-05-06 13:31:08 +0100

    Hotfix loading twitter credentials from either secrets file or environement variable

--- lib/twitter_client.rb
+++ lib/twitter_client.rb
@@ -4,6 +4,10 @@ class TwitterClient
   include Singleton
 
   def initialize
+    @twitter_consumer_key    = Rails.application.secrets.twitter_consumer_key || ENV["TWITTER_CONSUMER_KEY"]
+    @twitter_consumer_secret = Rails.application.secrets.twitter_consumer_secret || ENV["TWITTER_CONSUMER_SECRET"]
+    @twitter_access_token    = Rails.application.secrets.twitter_access_token || ENV["TWITTER_ACCESS_TOKEN"]
+    @twitter_access_secret   = Rails.application.secrets.twitter_access_secret || ENV["TWITTER_ACCESS_SECRET"]
     @client ||= initialize_client
   end
 
@@ -15,10 +19,10 @@ class TwitterClient
 
   def initialize_client
     Twitter::REST::Client.new do |config|
-      config.consumer_key        = Rails.application.secrets.twitter_consumer_key
-      config.consumer_secret     = Rails.application.secrets.twitter_consumer_secret
-      config.access_token        = Rails.application.secrets.twitter_access_token
-      config.access_token_secret = Rails.application.secrets.twitter_access_secret
+      config.consumer_key        = @twitter_consumer_key
+      config.consumer_secret     = @twitter_consumer_secret
+      config.access_token        = @twitter_access_token
+      config.access_token_secret = @twitter_access_secret
     end
   end
 end
